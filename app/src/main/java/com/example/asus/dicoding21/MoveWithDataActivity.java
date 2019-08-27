78509b4a2c718536ed586953c0392c910a189bc5
RkzG <franky_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2019-08-27 10:36:18 +0700

    My Intent App

---
+++ app/src/main/java/com/example/asus/dicoding21/MoveWithDataActivity.java
@@ -0,0 +1,32 @@
+package com.example.asus.dicoding21;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String EXTRA_AGE = "extra_age";
+    public static final String EXTRA_NAME = "extra_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+        TextView tvDataReceived = findViewById(R.id.tv_data_received);
+        tvDataReceived.setOnClickListener(this);
+
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+
+        String text = "Name : " + name + ",\nYour Age : " + age;
+        tvDataReceived.setText(text);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
